6d86ac5ba3d9a0f4142d0ada235686a9d65b6832
FireAEM <edric.huang.14@gmail.com>
2026-02-01 00:24:05 +0100

    feat(api-gateway) : add cors

--- services/api-gateway/src/app.js
+++ services/api-gateway/src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import productsRouter from "./routes/products.js";
 import ordersRouter from "./routes/orders.js";
 
 const app = express();
+
+app.use(cors());
 
 app.use("/products", productsRouter);
 app.use("/orders", ordersRouter);
